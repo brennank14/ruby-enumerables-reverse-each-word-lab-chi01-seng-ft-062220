adde8c64be7b728c0c6542b0a9a08388e85e3b18
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 20:58:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ sentence = "Hello my name is Kathleen"
 def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
   new_sentence = sentence_array.collect {|element| element.reverse}
-  puts new_sentence.join(" ")
+  new_sentence.join(" ")
 end
 
 reverse_each_word(sentence)
