1280deae0f434df07b739928c84470b6d619517c
Kathleen Brennan <brennank14@gmail.com>
2020-06-10 20:51:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,7 @@ def reverse_each_word(sentence)
   sentence_array = sentence.split(/ /)
   puts sentence_array
   new_sentence = sentence_array.each {|element| element.reverse}
+  puts new_sentence
 end
 
 reverse_each_word(sentence)
